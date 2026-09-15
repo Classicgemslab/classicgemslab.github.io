41495951b2190a9fe312821c740cd5e5228a4067
Classicgemslab <paraschadha9121@gmail.com>
2026-09-15 21:48:10 +0530

    Create certificates.js

---
+++ certificates.js
@@ -0,0 +1,17 @@
+const certificates = {
+
+    "CGL-451236": {
+        date: "September 14, 2026",
+        description: "Natural Diamond",
+        shape: "Round Brilliant",
+        measurements: "7.27 - 7.30 × 4.58 mm",
+        carat: "1.51 Carat",
+        color: "I",
+        clarity: "SI1-SI2",
+        cut: "Excellent",
+        polish: "Excellent",
+        fluorescence: "None",
+        pdf: "certificates/CGL-451236.pdf"
+    }
+
+};
