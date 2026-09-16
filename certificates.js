22dc7d08f83893470f32bf374528a55b04147a85
crowngemlab <paraschadha9121@gmail.com>
2026-09-16 16:12:36 +0530

    Update certificates.js

--- certificates.js
+++ certificates.js
@@ -13,5 +13,17 @@ const certificates = {
         fluorescence: "None",
         pdf: "certificates/CGL-451236.pdf"
     }
-
+ "CGL-451237": {
+        date: "September 14, 2026",
+        description: "Natural Diamond",
+        shape: "Round Brilliant",
+        measurements: "7.27 - 7.30 × 4.58 mm",
+        carat: "1.51 Carat",
+        color: "I",
+        clarity: "SI1-SI2",
+        cut: "Excellent",
+        polish: "Excellent",
+        fluorescence: "None",
+        pdf: "certificates/CGL-451237.pdf"
+    }
 };
